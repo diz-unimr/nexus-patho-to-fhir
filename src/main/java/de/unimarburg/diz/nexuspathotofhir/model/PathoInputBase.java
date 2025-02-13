41035405ab2ab92c77b6d9a938102d19dbdb148b
Jakub Lidke <jakub.lidke@uni-marburg.de>
2025-02-13 09:13:49 +0100

    refactor: add getUUID to PathoInputBase ; add SpecimenContainerTyp enum

--- src/main/java/de/unimarburg/diz/nexuspathotofhir/model/PathoInputBase.java
+++ src/main/java/de/unimarburg/diz/nexuspathotofhir/model/PathoInputBase.java
@@ -11,4 +11,6 @@ public interface PathoInputBase extends Serializable {
   String getAuftragsnummer();
 
   boolean isBaseValid();
+
+  String getUUID();
 }
---
+++ src/main/java/de/unimarburg/diz/nexuspathotofhir/model/SpecimenContainerTyp.java
@@ -0,0 +1,26 @@
+/* GNU AFFERO GENERAL PUBLIC LICENSE  Version 3 (C)2025 */
+package de.unimarburg.diz.nexuspathotofhir.model;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum SpecimenContainerTyp {
+  ROOT_CONTAINER(3),
+  SUB_CONTAINER(1),
+  MICROSCOPE_SLIDE(2);
+  private final int value;
+
+  SpecimenContainerTyp(int value) {
+    this.value = value;
+  }
+
+  public static SpecimenContainerTyp valueOf(int value) {
+    final Optional<SpecimenContainerTyp> first =
+        Arrays.stream(values()).filter(v -> v.value == value).findFirst();
+    if (first.isPresent()) return first.get();
+    else {
+      throw new IllegalArgumentException(
+          "container typ %s is not supported, yet.".formatted(value));
+    }
+  }
+}
